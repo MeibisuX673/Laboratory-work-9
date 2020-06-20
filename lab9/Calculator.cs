a602cbf4a83fec44199ebcd591930839baa0b02b
MeibisuX673 <g1eme.please@gmail.com>
2020-06-21 00:23:32 +0300

    Calculator

---
+++ lab9/Calculator.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab9
+{
+    
+        delegate void CalculatorUpdateOutput(Calculator sender, double value, int precision);
+        delegate void CalculatorInternalError(Calculator sender, string message);
+        enum CalculatorOperation { Add, Sub, Mul, Div }
+        enum CalculatorOperation2 { Sqrt, Sqr, Gip, Proc }
+        class Calculator
+        {
+            double? input = null;
+            double? result = null;
+            bool point = false;
+            int? count = null;
+
+            CalculatorOperation? op = null;
+            CalculatorOperation2? op2 = null;
+            public event CalculatorUpdateOutput didUpdateValue;
+            public event CalculatorInternalError InputError;
+            public event CalculatorInternalError EqullyError;
+            public event CalculatorInternalError MinusSqrtError;
+            public void AddDigit(int digit)
+            {
+                if (input.HasValue && Math.Log10(input.Value) > 9)
+                {
+                    InputError?.Invoke(this, "Value is too long for this calculator");
+                    return;
+                }
+                if (count > 8)
+                {
+                    InputError?.Invoke(this, "Value is too long for this calculator");
+                    return;
+                }
+                if (point)
+                {
+                    count = (count ?? 0);
+                    input = ((input ?? 0) * Math.Pow(10, (double)count) + (double)digit / 10) / Math.Pow(10, (double)count);
+                    didUpdateValue?.Invoke(this, input.Value, 0);
+                    count++;
+                }
+                else
+                {
+                    input = (input ?? 0) * 10 + digit;
+                    didUpdateValue?.Invoke(this, input.Value, 0);
+                }
+
+            }
+            public void AddOperation(CalculatorOperation op)
+            {
+
+                if (this.op.HasValue)
+                {
+                    Compute();
+                }
+
+
+                this.op = op;
+                if (input.HasValue)
+                {
+                    result = input;
+                    this.Clear();
+                }
+
+
+            }
+
+            public void Compute()
+            {
+                switch (this.op)
+                {
+                    case CalculatorOperation.Add:
+                        result = result + (input ?? 0);
+                        didUpdateValue?.Invoke(this, result.Value, 0);
+                        input = null;
+                        break;
+                    case CalculatorOperation.Div:
+                        if (input != null && input.Value != 0)
+                        {
+                            result = result / (input ?? 0);
+                            didUpdateValue?.Invoke(this, result.Value, 0);
+                            input = null;
+                            break;
+                        }
+                        else
+                        {
+                            EqullyError?.Invoke(this, "Division by zero");
+                            break;
+
+                        }
+                    case CalculatorOperation.Mul:
+                        result = result - (input ?? 0);
+                        didUpdateValue?.Invoke(this, result.Value, 0);
+                        input = null;
+                        break;
+                    case CalculatorOperation.Sub:
+                        result = result * (input ?? 0);
+                        didUpdateValue?.Invoke(this, result.Value, 0);
+                        input = null;
+                        break;
+
+
+
+                }
+
+            }
+
+            public void AddOperation2(CalculatorOperation2 op2)
+            {
+
+                this.op2 = op2;
+                if (input.HasValue)
+                {
+
+                    ComOperation2();
+                }
+
+            }
+
+            public void ComOperation2()
+            {
+                switch (this.op2)
+                {
+                    case CalculatorOperation2.Sqrt:
+                        if (input < 0)
+                        {
+                            InputError?.Invoke(this, "Negative number under the root");
+                            return;
+                        }
+                        input = Math.Sqrt(input ?? 0) * 1.0;
+                        didUpdateValue?.Invoke(this, input.Value, 0);
+                        break;
+                    case CalculatorOperation2.Sqr:
+                        input = Math.Pow((input ?? 0), 2);
+                        didUpdateValue?.Invoke(this, input.Value, 0);
+                        break;
+                    case CalculatorOperation2.Gip:
+                        input = 1.0 / (input ?? 0);
+                        didUpdateValue?.Invoke(this, input.Value, 0);
+                        break;
+                    case CalculatorOperation2.Proc:
+                        input = ((result ?? 0) * input) / 100;
+                        didUpdateValue?.Invoke(this, input.Value, 0);
+                        break;
+                }
+
+            }
+
+            public void Clear()
+            {
+                input = null;
+
+                didUpdateValue?.Invoke(this, 0, 0);
+            }
+            public void ClearAll()
+            {
+                input = null;
+                result = null;
+                op2 = null;
+                op = null;
+                didUpdateValue?.Invoke(this, 0, 0);
+            }
+            public void Point()
+            {
+                point = true;
+            }
+            public void Equlay()
+            {
+                op = null;
+                op2 = null;
+                input = result;
+
+            }
+            public void Polar()
+            {
+                input = -(input);
+                didUpdateValue?.Invoke(this, input.Value, 0);
+            }
+
+            public void ClearEnd()
+            {
+                if (point)
+                {
+                    input = (int)(((input ?? 0) * Math.Pow(10, (int)count) / 10)) / Math.Pow(10, (int)count - 1);
+                    count--;
+                    if (count == 0)
+                        point = false;
+                    didUpdateValue?.Invoke(this, input.Value, 0);
+                }
+                else
+                {
+                    input = (int)((input ?? 0) / 10);
+                    didUpdateValue?.Invoke(this, input.Value, 0);
+                }
+
+            }
+            public void CountPointOff()
+            {
+                count = null;
+                point = false;
+            }
+        }
+}
+
---
+++ lab9/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calc;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calc = new Calculator();
+            calc.didUpdateValue += CalculatorDidUpdateValue;
+            calc.InputError += CalculatorInputError;
+            calc.EqullyError += CalculatorInternalError;
+            calc.MinusSqrtError += CalculatorMinusError;
+            calc.Clear();
+        }
+        private void CalculatorMinusError(Calculator sender, string message)
+        {
+            MessageBox.Show(message, "Error");
+        }
+
+        private void CalculatorInputError(Calculator sender, string message)
+        {
+            MessageBox.Show(message, "Error");
+        }
+
+        private void CalculatorDidUpdateValue(Calculator sender, double value, int precision)
+        {
+            label1.Content = value.ToString();
+        }
+
+        private void CalculatorInternalError(Calculator sender, string message)
+        {
+            MessageBox.Show(message, "Error");
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int digit = -1;
+            Button b = sender as Button;
+            if (int.TryParse(b.Content.ToString(), out digit))
+            {
+
+                calc.AddDigit(digit);
+            }
+            else
+            {
+                switch (b.Name)
+                {
+
+                    case "minus":
+                        calc.CountPointOff();
+                        calc.AddOperation(CalculatorOperation.Mul);
+
+                        break;
+                    case "plus":
+                        calc.AddOperation(CalculatorOperation.Add);
+                        calc.CountPointOff();
+                        break;
+                    case "clear":
+                        calc.CountPointOff();
+                        calc.Clear();
+                        break;
+                    case "div":
+                        calc.AddOperation(CalculatorOperation.Div);
+                        calc.CountPointOff();
+                        break;
+                    case "mult":
+                        calc.AddOperation(CalculatorOperation.Sub);
+                        calc.CountPointOff();
+                        break;
+                    case "equally":
+
+                        calc.Compute();
+                        calc.Equlay();
+                        calc.CountPointOff();
+                        break;
+                    case "ClearAll":
+                        calc.ClearAll();
+                        calc.CountPointOff();
+                        break;
+                    case "point":
+
+                        this.label1.Content += ",";
+                        calc.Point();
+                        break;
+                    case "sqrt":
+                        calc.CountPointOff();
+                        calc.AddOperation2(CalculatorOperation2.Sqrt);
+                        break;
+                    case "sqr":
+                        calc.CountPointOff();
+                        calc.AddOperation2(CalculatorOperation2.Sqr);
+                        break;
+                    case "gip":
+                        calc.CountPointOff();
+                        calc.AddOperation2(CalculatorOperation2.Gip);
+                        break;
+                    case "polar":
+                        calc.CountPointOff();
+                        calc.Polar();
+                        break;
+                    case "clearEnd":
+                        calc.ClearEnd();
+                        break;
+                    case "procent":
+                        calc.CountPointOff();
+                        calc.AddOperation2(CalculatorOperation2.Proc);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+        }
+
+    }
+}
